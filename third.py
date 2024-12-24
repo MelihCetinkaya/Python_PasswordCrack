8deebb010ae1492d8856955c76c3a0dd991e5d9a
Melih Çetinkaya <melih07160716@gmail.com>
2024-12-24 20:40:28 +0300

    ilk

---
+++ third.py
@@ -0,0 +1,23 @@
+ 
+# try :  
+#    number=int(input("please enter a number"))
+#    print(1/number)
+# # except ValueError:
+# #    print("enter a valid number")
+# # except ZeroDivisionError:
+# #    print("you can't divide a number by zero")    
+
+# except Exception as e :
+#       print("an error has occurred :+{e}")
+#       print(e)
+#       print(type(e).__name__)
+      
+# else :
+#    print("ok")    
+   
+   
+# finally :
+#     print("no matter what it is")    
+
+
+ 
